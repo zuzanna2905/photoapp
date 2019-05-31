7e97a826d03135d582c0cefce2c27688c6114380
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-31 15:47:32 +0200

    set welcome page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'donotreplay@example.com'
+  config.mailer_sender = 'donotreplay@mail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
